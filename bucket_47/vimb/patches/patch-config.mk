dce225fa71181518f203055016785eead2857cdf
John Marino <draco@marino.st>
2019-01-29 08:23:21 -0600

    vimb: fix web extension linkage and strip binaries

--- bucket_47/vimb/patches/patch-config.mk
+++ bucket_47/vimb/patches/patch-config.mk
@@ -1,8 +1,6 @@
-diff --git config.mk config.mk
-index 5691aae..081f667 100644
---- config.mk
+--- config.mk.orig	2018-11-06 22:27:25 UTC
 +++ config.mk
-@@ -10,6 +10,7 @@ DOTDESKTOPPREFIX := $(DESTDIR)$(PREFIX)/share/applications
+@@ -12,6 +12,7 @@ DOTDESKTOPPREFIX := $(DESTDIR)$(PREFIX)/
  LIBDIR           := $(DESTDIR)$(PREFIX)/lib/vimb
  RUNPREFIX        := $(PREFIX)
  EXTENSIONDIR     := $(RUNPREFIX)/lib/vimb
@@ -10,9 +8,9 @@ index 5691aae..081f667 100644
  
  # define some directories
  SRCDIR  = src
-@@ -22,12 +23,15 @@ LIBS = gtk+-3.0 'webkit2gtk-4.0 >= 2.20.0'
+@@ -26,17 +27,20 @@ COMMIT := $(shell git describe --tags --
  CFLAGS   += -std=c99 -pipe -Wall -fPIC
- CPPFLAGS += -DEXTENSIONDIR=\"${EXTENSIONDIR}\"
+ CPPFLAGS += -DVERSION=\"${VERSION}\" -DEXTENSIONDIR=\"${EXTENSIONDIR}\" -DCOMMIT=\"$(COMMIT)\"
  CPPFLAGS += -DPROJECT=\"vimb\" -DPROJECT_UCFIRST=\"Vimb\"
 -CPPFLAGS += -D_XOPEN_SOURCE=500
 -CPPFLAGS += -D__BSD_VISIBLE
@@ -28,3 +26,9 @@ index 5691aae..081f667 100644
  # flags used to build webextension
  EXTTARGET   = webext_main.so
  EXTCFLAGS   = ${CFLAGS} $(shell pkg-config --cflags webkit2gtk-web-extension-4.0)
+ EXTCPPFLAGS = $(CPPFLAGS)
+-EXTLDFLAGS  = $(shell pkg-config --libs webkit2gtk-web-extension-4.0) -shared
++EXTLDFLAGS  = -Wl,-rpath,$(PREFIX)/lib $(shell pkg-config --libs webkit2gtk-web-extension-4.0) -shared
+ 
+ # flags used for the main application
+ CFLAGS     += $(shell pkg-config --cflags $(LIBS))
