2e57ecad16f33113d7a3e75f9e1e281df86e9ae8
John Marino <draco@marino.st>
2020-06-24 15:19:17 +0000

    work on libuuid: requires patches to all OS

---
+++ bucket_46/libuuid/patches/patch-lib_randutils.c
@@ -0,0 +1,16 @@
+O_CLOEXEC is not available on every platform (e.g. MacOS X < 10.7). It
+was introduced in POSIX 2008.
+
+--- lib/randutils.c.orig	2018-03-20 13:16:23 UTC
++++ lib/randutils.c
+@@ -74,6 +74,10 @@ static void crank_random(void)
+ 		rand();
+ }
+ 
++#if !defined(O_CLOEXEC)
++#define O_CLOEXEC 0
++#endif
++
+ int random_get_fd(void)
+ {
+ 	int i, fd;
